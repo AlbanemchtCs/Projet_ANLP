20988e02ffc86b5561095180675584a06ecf7c3d
Rodolphe Nonclercq <rodolphe.nonclercq@student-cs.fr>
2023-03-19 19:11:17 +0100

    add pond and test result

--- train_test.py
+++ train_test.py
@@ -12,7 +12,7 @@ from torch.utils.data import DataLoader
 from transformers import BertTokenizer,BertForSequenceClassification,AutoModelForSequenceClassification, AdamW
 from progressbar import progressbar
 from tqdm import tqdm
-
+import seaborn as sns
 def encode_text_dataset(dataset, model_name, output_dir):
     # Charger le modèle mBERT pré-entraîné
     tokenizer = BertTokenizer.from_pretrained(model_name)
@@ -32,7 +32,7 @@ def encode_text_dataset(dataset, model_name, output_dir):
 
 
 def tok_dataset(dataset,tokenizer):
-    toked = dataset.map(tok)
+    toked = dataset.map(lambda x :  tok(x,tokenizer))
     dic = {}
     dic['input_ids']= toked['input_ids']
     dic['attention_mask']= toked['attention_mask']
@@ -48,16 +48,29 @@ BATCH_SIZE = 32
 EPOCH = 1
 
 
-def tok(examples):
+def tok(examples,tokenizer):
     return tokenizer(examples['Utterance'], padding="max_length", max_length=MAX_LENGTH,truncation=True)
 
 
-def exec_train(model,tokenizer,dataset):
+def exec_train(model,tokenizer,dataset,pond=False):
     #use device
     device  = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model.to(device)
     tok_train = tok_dataset(dataset,tokenizer)
     data_train = DataLoader(tok_train,batch_size=BATCH_SIZE,shuffle=True)
+    nb_classes = len(set(dataset['Label']))
+    if pond:
+        class_weights = np.array([0 for i in range(nb_classes)])
+        for i in dataset['Label']:
+            class_weights[i] += 1
+        max_nb_class = class_weights.max()
+        class_weights = np.power(max_nb_class/class_weights,0.25)
+        class_weights =  torch.Tensor(class_weights).to(device)
+        print(class_weights)
+    else:
+        class_weights = torch.Tensor([1. for i in range(nb_classes)]).to(device)
+    
+    criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
     optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
     #Training
     for epoch in range(EPOCH):
@@ -76,7 +89,9 @@ def exec_train(model,tokenizer,dataset):
             nb_samples += BATCH_SIZE
             
             #loss
-            loss = outputs.loss
+            #loss = outputs.loss
+            logits = outputs['logits']
+            loss = criterion(logits, batch['labels'])
             loss_tot += float(loss)
             
             #accuracy
@@ -94,11 +109,15 @@ def exec_train(model,tokenizer,dataset):
             pbar.update(1)
     #return model
 
-def exec_test(model,tokenizer,dataset):
+def exec_test(model,tokenizer,dataset,nb_label=None):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model.to(device)
     tok_test = tok_dataset(dataset,tokenizer)
     data_test = DataLoader(tok_test,batch_size=BATCH_SIZE,shuffle=False)
+    if nb_label is None:
+        nb_label = np.max(dataset['Label'])+1
+    acc_matrix = np.zeros((nb_label,nb_label))
+    
     #validation
     model.eval()
     with torch.no_grad():
@@ -114,12 +133,15 @@ def exec_test(model,tokenizer,dataset):
             outputs = model(**batch)
             logits = outputs.logits
             predictions = torch.argmax(logits, dim=-1)
+            for i in range(len(batch['labels'])):
+                acc_matrix[int(batch['labels'][i]),int(predictions[i])] += 1
             total_correct += (predictions == batch['labels']).sum().item()
             total_samples += batch['labels'].size(0)
             pbar.set_postfix(accuracy=total_correct/total_samples)
             pbar.update(1)
         val_acc = total_correct / total_samples
         print('test Acc:', val_acc)
+    return acc_matrix
 
 """
 ###EXAMPLE###
@@ -133,8 +155,12 @@ num_labels = len(set(dataset['train']['Label']))
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', return_tensors='pt')
 model = BertForSequenceClassification.from_pretrained('bert-base-multilingual-cased',num_labels=num_labels, problem_type="single_label_classification")
 
-exec_train(model,tokenizer,datasets.concatenate_datasets([dataset['train'],dataset['validation']]))
-exec_test(model,tokenizer,dataset['test'])
-
+exec_train(model,tokenizer,datasets.concatenate_datasets([dataset['train'],dataset['validation']]),True)
+M = exec_test(model,tokenizer,dataset['test'])
+w = np.sum(M,axis=1)
+for i in range(w.shape[0]):
+    if w[i] == 0:
+        w[i] = 1
+sns.heatmap(M/w)
 """
 
